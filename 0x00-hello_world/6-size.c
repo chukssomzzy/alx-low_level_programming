371ae4c30d2c2f20e19199914549a84b640fa9a3
chukssomzzy <chukssomzzy19@gmail.com>
2022-09-09 07:54:07 +0200

    fixed size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main -Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * * main -Entry point
+ * 
+ * * Return: Always 0 (Success)
+ *     
+ */
 
 int main(void)
 
@@ -20,6 +21,6 @@ int main(void)
 
         printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-                    return (0);
+    return (0);
 
 }
