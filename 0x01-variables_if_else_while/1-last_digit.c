540d88e9c0174555976585957e7e13a6343bf3f2
chukssomzzy <chukssomzzy@gmail.com>
2022-09-10 08:20:26 +0100

    typing error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %i is %i and is less than 5 and not 0\n", n, lastN);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastN);
 	}
 	return (0);
 }
