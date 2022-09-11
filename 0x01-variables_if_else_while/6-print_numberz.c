2d88449b663d9920b821eec26aca06d40a8d2100
chukssomzzy <chukssomzzy@gmail.com>
2022-09-11 02:32:34 +0100

    done 5 and 6 with one code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+ # include <stdio.h>
+
+/**
+ * * main - entry point of the program
+ *
+ * * Return: success(0)
+ */
+
+int main(void)
+{
+	int numChar = 48;
+
+	while (numChar < 48 + 10)
+	{
+		putchar(numChar);
+		numChar++;
+	}
+	putchar('\n');
+	return (0);
+}
