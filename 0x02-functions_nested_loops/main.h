6a002d6f3768338beefd0f07f2201cc09db898d3
chukssomzzy <chukssomzzy@gmail.com>
2022-09-13 23:11:25 +0100

    added declaration to main.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,1 @@
- # include <stdio.h>
-
-/**
- * _putchar - put a char in console
- * @_tmp: an interger value to translate to char from ascii
- * Return: succes (0)
- */
-
-void _putchar(int _tmp)
-{
-	putchar(_tmp);
-}
+int _putchar(char c);
