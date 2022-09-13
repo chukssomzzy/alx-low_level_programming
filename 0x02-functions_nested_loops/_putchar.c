cf290964768beda5e05d0ada55bb713789121981
chukssomzzy <chukssomzzy@gmail.com>
2022-09-13 23:00:33 +0100

    done with the task

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/**
+ * _putchar - put a char in console
+ * @_tmp: an interger value to translate to char from ascii
+ * Return: succes (0)
+ */
+
+void _putchar(int _tmp)
+{
+	putchar(_tmp);
+}
