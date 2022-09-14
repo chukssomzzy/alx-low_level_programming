3974f309bc4e356643d4b8e84bad37d853657bb5
chukssomzzy <chukssomzzy19@gmail.com>
2022-09-14 07:15:39 +0200

    fellow betty doc and style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,12 +4,13 @@
  * add - add two params
  * @n1 parmas 1
  * @n2 param 2
- *Description: no side effect
- * Return: rhe sum of the two params
+ * Description: no side effect
+ * Return: sum of the two params
  */
 
 int add(int n1, int n2)
 {
 	int sum = n1 + n2;
+
 	return (sum);
 }
