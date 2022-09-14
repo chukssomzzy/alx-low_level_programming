6b305a5cf88ed9a6cdc1eb02ade6aaa0b2be3070
chukssomzzy <chukssomzzy@gmail.com>
2022-09-14 03:57:16 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+# include "main.h"
+
+/**
+ * _abs - returns the unsigned value of a params
+ * @n: interger value of n
+ * Return: unsigned int
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+
+	return (n);
+
+}
