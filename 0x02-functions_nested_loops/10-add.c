8f49da133eeaeaf9702242571e18ca7234960c5a
chukssomzzy <chukssomzzy19@gmail.com>
2022-09-14 07:13:25 +0200

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+# include "main.h"
+
+/**
+ * add - add two params
+ * @n1 parmas 1
+ * @n2 param 2
+ *Description: no side effect
+ * Return: rhe sum of the two params
+ */
+
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+	return (sum);
+}
