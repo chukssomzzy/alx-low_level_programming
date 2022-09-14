5ae2ac285a48222ce4e1154af6ca51e21ea66413
chukssomzzy <chukssomzzy@gmail.com>
2022-09-14 02:01:39 +0100

    chaged the function name

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
  # include "main.h"
 /**
- * print_alphabet - print the alphabet in lowercase
+ * print_alphabet_x10 - print the alphabet in lowercase
  *
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int n;
+
 	ch = 'a';
 	n = 0;
 	while (ch <= 'z' && n < 10)
