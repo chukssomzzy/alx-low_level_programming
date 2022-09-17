1efb2463caa3a29ac05cbd50a50b5ca1f2b3ed7d
chukssomzzy <chukssomzzy@gmail.com>
2022-09-17 01:08:31 +0100

    done with task

---
+++ 0x04-more_functions_nested_loops/4-print_most_number.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/**
+ * print_most_numbers - print all number except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		if (c == '2' || c == '4')
+			continue;
+		_putchar(c++);
+	}
+	_putchar('\n');
+}
