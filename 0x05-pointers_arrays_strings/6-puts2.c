653549da098c61a7313a0d9521903a4917dc5077
chukssomzzy <chukssomzzy@gmail.com>
2022-09-20 03:02:25 +0100

    i used curlys

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,12 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (*(str + ++i) != '\0')
+	while (*(str + i) != '\0')
+	{
 		if (i % 2 == 0)
 			_putchar(*(str + i));
+		i++;
+	}
 	if (*(str + i) == '\0')
 		_putchar('\n');
 }
