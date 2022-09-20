24416faf2af67bf12b87224db7df2aff19cf7e87
chukssomzzy <chukssomzzy@gmail.com>
2022-09-20 02:16:09 +0100

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * rev_string - reverse a string
+ * @s: pointer variable
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char tmp[1000];
+
+	j = 0;
+	for (i = 0; (*(tmp + i) = *(s + i)) != '\0'; i++)
+		;
+	while (i > 0)
+		*(s + j++) = *(tmp + --i);
+}
