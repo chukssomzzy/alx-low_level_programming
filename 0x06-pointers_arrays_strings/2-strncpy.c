45f6ad71712cda37ec1f04535844df69ba9b75ea
chukssomzzy <chukssomzzy19@gmail.com>
2022-09-22 05:04:00 -0700

    fone

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && (*(dest + i) = *(src + i)) != '\0'; i++)
-		;
-	return (dest);
+	for (i = 0; i < n; i++)
+		if (*(src + i) != '\0')
+			*(dest + i) = *(src + i);
+		else
+			break;
+	*(dest + i)  = '\0';
+
+		return (dest);
 }
