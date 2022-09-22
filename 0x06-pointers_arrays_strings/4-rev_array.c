7e3b26826c467f7321369661d90ad8d0e151f56f
chukssomzzy <chukssomzzy19@gmail.com>
2022-09-22 17:12:09 +0100

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * reverse_array - reverse an array of intergers
+ * @a: array
+ * @n: number of place to reverse
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+	int tmp[1000];
+    i = 0;
+	j = n - 1;
+
+	while (i < n)
+	{
+		*(tmp + i) = *(a + i);
+		i++;
+	}
+		;
+	i = 0;
+	while (i < n)
+		*(a + i++) = *(tmp + j--);
+}
