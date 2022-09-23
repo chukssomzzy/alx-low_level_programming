79ebac645271c50a65b46a18c891b03457597145
chukssomzzy <chukssomzzy19@gmail.com>
2022-09-23 03:57:28 +0100

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,37 @@
+# include "main.h"
+# define BASE 10
+/**
+ * print_number - print numbers
+ * @number: temporary var of number to print
+ */
+
+
+
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
