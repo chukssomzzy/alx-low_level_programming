31bfb15bb79d81ee08d5f5def11847e4deb756be
chukssomzzy <chukssomzzy19@gmail.com>
2022-09-26 17:41:52 +0100

    betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 # include "main.h"
 
 /**
- * memset - fills a buffer with a const char
- * @j: pointer to a string
+ * _memset - fills a buffer with a const char
+ * @s: pointer to a string
  * @b: a dynamic variable of a char
  * @n: an unsigned int holding value of size of value to fill up
  */
@@ -10,7 +10,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-	while (n-- != 0 && (*(s++) = b) == b)
+	while (--n != 0 && (*(s++) = b) == b)
 		;
 	return (s);
 }
