9350d1fd779d34436c171346fec069145ae9d240
chukssomzzy <chukssomzzy19@gmail.com>
2022-09-30 11:20:45 +0100

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+# include "main.h"
+# include <stdlib.h>
+# include <stdio.h>
+
+/**
+ * main - print the program name
+ * @argc: hold the len of arg vector
+ * @argv: arg vector (1 dimensional array)
+ * Return: success or failure
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	exit(EXIT_SUCCESS);
+}
