157039a4d97b44a7263d4a7488466a3221dd301c
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-03 15:15:35 +0100

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+# include "main.h"
+# include <stdlib.h>
+
+/**
+ * create_arrayddress range perms: large range  - create an array of char
+ * @size: size of array
+ * @c: ascii decimal value of char
+ * Return: pointer or NULl
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	s = malloc(sizeof(*s) * size);
+	i = 0;
+	if (size == 0)
+		return (NULL);
+	while (size-- > 0)
+		*(s + i++) = c;
+	return (s);
+}
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,10 @@
+# include <unistd.h>
+/**
+ * _putchar - prints a character to stdoutput
+ * @c: holds variable for the character to print
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
