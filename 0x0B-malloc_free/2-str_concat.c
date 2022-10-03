045c9a058e1ead84dbbdc30c3beb1bace66378bd
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-03 18:21:40 +0100

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 
 int _strsize(char *s);
 /**
- * _strconcat - joins two string
+ * str_concat - joins two string
  * @s1: first string
  * @s2: second string
  * Return: Null all pointer to the string joined
@@ -11,21 +11,24 @@ int _strsize(char *s);
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	int i, j, k;
 	char *s;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
-	i = _strsize(s1);
-	j = _strsize(s2);
-	s = malloc(sizeof(char) * (i + j));
+	i = s1 ? _strsize(s1) : 0;
+	j = s2 ? _strsize(s2) : 0;
+	s = malloc((sizeof(char) * (i + j)) + 1);
 	if (!s)
+	{
+		free(s);
 		return (NULL);
-	i = 0;
-	while ((*(s + i) = *(s1 + i)) != '\0')
-		i++;
-	for (j = 0; (*(s + i) = *(s2 + j)) != '\0'; i++, j++)
-		;
+	}
+	for (k = 0; k < i; k++)
+		*(s + k) = *(s1 + k);
+	i = k;
+	for (j = 0; j <= i; j++, k++)
+		*(s + k) = *(s2 + j);
 	return (s);
 }
 
@@ -40,7 +43,7 @@ int _strsize(char *s)
 	int i;
 
 	i = 0;
-	while (*(s + i++) != '\0')
-		;
+	while (*(s + i) != '\0')
+		i++;
 	return (i);
 }
