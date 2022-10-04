c4fad63fb8745bbb824af57ab38634c281ff27fb
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-04 14:46:25 +0100

    dynamic array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+# include "main.h"
+# include <stdlib.h>
+
+
+/**
+ * **alloc_grid - makes a 2d array of 0
+ * @w: width of array or column
+ * @h: height of array or row
+ * Return: a pointer to the array
+ */
+
+int **alloc_grid(int h, int w)
+{
+	int **a, i, j;
+
+	if (h <= 0 || w <= 0)
+		return (NULL);
+	a = malloc(sizeof(int *) * h);
+	if (!a)
+	{
+		free(a);
+		return (NULL);
+	}
+	i = 0;
+	while (i < w)
+	{
+		a[i] = malloc(sizeof(int) * w);
+		if (!*(a + i))
+		{
+			free(a);
+			return (NULL);
+		}
+		i++;
+	}
+	for (i = 0; i < h; i++)
+		for(j = 0; j < w; j++)
+			a[j][i] = 0;
+	return (a);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 char *create_array(unsigned int, char);
 char *_strdup(char *str);
 char *str_concat(char *, char *);
+int **alloc_grid(int w, int h);
