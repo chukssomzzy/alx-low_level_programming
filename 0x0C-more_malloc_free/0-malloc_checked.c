817118a4e840115a98949b531f365bcb6c52ab11
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-06 19:03:53 +0100

    where the fu** is the error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,14 +4,15 @@
 /**
  * malloc_checked - allocate memory using malloc
  * @b: holds amount of bytes to allocate from heap
+ * Return: pointer or exit null
  */
 
 void *malloc_checked(unsigned int b)
 {
-	char *a;
+	void *a;
 
 	a = malloc(b);
 	if (!a)
-		exit (98);
+		exit(98);
 	return (a);
 }
