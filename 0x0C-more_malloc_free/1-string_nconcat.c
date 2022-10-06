99c85c691c1cfaea39b1a4f70bb0aa16c1b52b1f
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-06 22:04:08 +0100

    cat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+# include "main.h"
+# include <stdlib.h>
+
+/**
+ * string_nconcat - concat two strings
+ * @s1: The first strings
+ * @s2: second string pointer
+ * @n: unsigned interger indicating number of bytes to copy
+ * Return: a pointer or null
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p, i, j, tmp;
+
+	if (!s1 || !s2)
+		return (NULL);
+	p = malloc((sizeof *s1) + n);
+	if (!p)
+	{
+		free(p);
+		return (NULL);
+	}
+	i = 0;
+	while ((*(p + i) = *(s1 + i)) != '\0')
+		i++;
+	j = 0;
+	tmp = n;
+	while (tmp-- > 0 && (*(p + i) = *(s2 + j++)) != '\0')
+		i++;
+	if (*(p + i++) != '\0')
+		*(p + i) = '\0';
+	return (p);
+}
