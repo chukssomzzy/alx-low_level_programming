f429a03067e84ee6757bbc0d7122230cace8088d
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-07 07:05:40 +0100

    moved again

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,24 +11,30 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p, i, j, tmp;
+	char *p, i, len, s1_len, s2_len;
 
-	if (!s1 || !s2)
-		return (NULL);
-	p = malloc((sizeof(*s1)) + n - 1);
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	i = len = s1_len = s2_len = 0;
+	while (*(s1 + s1_len) != '\0');
+		s1_len++;
+	while (*(s2 + s2_len) != '\0')
+		s2_len++;
+	if (n > s2_len)
+		n = s2_len;
+	len = s1_len + n;
+	p = malloc(len + 1);
 	if (!p)
 	{
 		free(p);
 		return (NULL);
 	}
-	i = 0;
-	while ((*(p + i) = *(s1 + i)) != '\0')
-		i++;
-	j = 0;
-	tmp = n;
-	while (tmp-- > 0 && (*(p + i) = *(s2 + j++)) != '\0')
-		i++;
-	if (*(p + i++) != '\0')
-		*(p + i) = '\0';
+	for (i = 0; i < len; i++)
+		if (i < s1_len)
+			*(p + i) = *(s1 + i);
+		else
+			*(p + i) = *(s2 + i - s1_len);
 	return (p);
 }
