06d80e7d44f4ce22849136f8b09423bf8c251e4a
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-07 16:40:10 +0100

    betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,6 +6,7 @@
  * @ptr: pointer to memory to be changed
  * @old_size: size of old memory
  * @new_size: new size to be allocated
+ * Return: pointer or null
  */
 
 
@@ -13,11 +14,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
 	char *tmp_ptr;
-	char *cast_ptr = (char*) ptr;
+	char *cast_ptr = (char *) ptr;
 
 	if (new_size == old_size)
 		return (ptr);
-    tmp_ptr = malloc(new_size);
+	tmp_ptr = malloc(new_size);
 	if (!ptr)
 		return (tmp_ptr);
 	else if (!new_size)
@@ -25,12 +26,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-    if (!tmp_ptr)
+	if (!tmp_ptr)
 	{
 		free(tmp_ptr);
 		return (NULL);
 	}
-    i = 0;
+	i = 0;
 	while (i < old_size)
 	{
 		*(tmp_ptr + i) = *(cast_ptr + i);
