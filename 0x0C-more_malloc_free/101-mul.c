4782199a692b66ea25defc27dc8bdc46688af09e
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-07 23:24:11 +0100

    myl

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+# include "main.h"
+
+/**
+ * main - multiply two positive number
+ * @argc: holds the size of argv to size of type of argv
+ * @argv: hold args passed to main during execution
+ *
+ * Description: Multiple two positive numbers and if the first one any of
+ * them contains a non-positive number or string returns an Error and a newline
+ * Example:
+ * 101-mul 20 40 = 800\n
+ * Return: 0 on success and 98 on failure
+ */
