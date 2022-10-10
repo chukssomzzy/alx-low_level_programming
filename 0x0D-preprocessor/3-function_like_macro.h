68d32392c628965da9d3dc7172c5759dbdb6e4aa
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-10 16:40:53 +0100

    SUM

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 # ifndef _FUNC_MACRO_H
 # define _FUNC_MACRO_H
-# define ABS(X) ((X < 0) ? (X * -1) : (X))
+# define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
 # endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef _MAIN_SUM_H
+#define _MAIN_SUM_H
+#define SUM(x, y) ((x) + (y))
+
+#endif
