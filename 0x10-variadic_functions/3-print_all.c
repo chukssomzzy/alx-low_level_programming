3758c53feb0c164ba868175f0044c741babe4b17
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-14 16:19:03 +0100

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,11 @@
 # include "variadic_functions.h"
-# include <stdarg.h>
 # include <stdio.h>
+# include <stdarg.h>
+
+void print_string(va_list);
+void print_char(va_list);
+void print_float(va_list);
+void print_int(va_list);
 
 /**
  * print_all - print variadic by knowing the format string
@@ -13,34 +18,94 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
+	int i, j;
+	int fmt_size;
+	fmt_t fun_fmts[] = {
+		{'c', print_char},
+		{'f', print_float},
+		{'i', print_int},
+		{'s', print_string}
+	};
+	fmt_size = 4;
+	va_start(ap, format);
+	for (i = 0; format && *(format + i); i++)
+		for (j = 0; j < fmt_size; j++)
+			if ((*(format + i) == ((fun_fmts + j)->fmt)))
+			{
+				(*(fun_fmts + j)->print_format)(ap);
+				if (*(format + i + 1) != '\0')
+					printf(", ");
+				break;
+			}
+	printf("\n");
+	va_end(ap);
+}
+
+/**
+ * print_char - prints char to stdout
+ * @aps: pointer to variadic function
+ *		 that hold position of the current argument
+ *
+ * Description: print char to stdout and returns nothing
+ */
+
+void print_char(va_list aps)
+{
+	char c;
+
+	c = va_arg(aps, int);
+	printf("%c", c);
+}
+
+/**
+ * print_float - prints char to stdout
+ * @aps: pointer to variadic function
+ *		 that hold position of the current argument
+ *
+ * Description: print float to stdout and returns nothing
+ */
+
+void print_float(va_list aps)
+{
+	float i;
+
+	i = va_arg(aps, double);
+	printf("%f", i);
+}
+
+/**
+ * print_int - prints int to stdout
+ * @aps: pointer to variadic function that
+ *			hold position of the current argument
+ *
+ * Description: print int to stdout and returns nothing
+ */
+
+void print_int(va_list aps)
+{
 	int i;
 
-	i = 0;
-	va_start(ap, format);
-	while (*(format + i) != '\0')
-		switch (*(format + i++))
-		{
-			case 'c':
-				printf("%i", va_arg(ap, int));
-				if (*(format + i) != '\0')
-					printf(", ");
-				break;
-			case 'i':
-				printf("%i", va_arg(ap, int));
-				if (*(format + i) != '\0')
-					printf(", ");
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double));
-				if (*(format + i) != '\0')
-					printf(", ");
-				break;
-			case 's':
-				printf("%s", va_arg(ap, char *));
-				if (*(format + i) != '\0')
-					printf(", ");
-				break;
-		}
-	va_end(ap);
-	printf("\n");
+	i = va_arg(aps, int);
+
+	if (i)
+		printf("%i", i);
 }
+
+/**
+ * print_string - prints char to stdout
+ * @ap: pointer to variadic function that hold position of the current argument
+ *
+ * Description: print char to stdout and returns nothing
+ */
+
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (s)
+		printf("%s", s);
+	else
+		printf("(nil)");
+}
+
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,4 +12,25 @@ int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *, const unsigned int, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct format - holds the fmt string and fmt function
+ * @fmt: the fmt string
+ * @print_format: format function
+ *
+ * Description: data structure for print_all
+ */
+
+struct format
+{
+	char fmt;
+	void (*print_format)(va_list);
+};
+
+/*
+ * fmt_t - type alias for struct format
+ */
+
+typedef struct format fmt_t;
+
 #endif
