cd6f82443e1f7ee434e72210d215d09724e4e868
chukssomzzy <chukssomzzy19@gmail.com>
2022-10-24 19:10:03 +0100

    ==free2==

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	tmp = *head;
-	if (!(*head))
-		return;
-	free(*head);
-	while ((*tmp).next)
-		free((tmp = tmp->next));
-	free(tmp);
-	*head = NULL;
+	if (head)
+	{
+		tmp = *head;
+		while ((*tmp).next)
+		{
+			free(tmp);
+			tmp = tmp->next;
+		}
+		free(tmp);
+		*head = NULL;
+	}
 }
