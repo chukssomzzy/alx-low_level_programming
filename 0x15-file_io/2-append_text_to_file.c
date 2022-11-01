a8112b7998cef89c63ead9eae29f9564b183a1b7
chukssomzzy <chukssomzzy19@gmail.com>
2022-11-01 14:39:47 +0100

    permission

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	cnt = _strlen(text_content);
 	if (cnt == -1)
 		cnt = 0;
-	ocnt = open(filename, O_WRONLY | O_APPEND, 664);
+	ocnt = open(filename, O_WRONLY | O_APPEND);
 	if (ocnt == -1)
 		return (ocnt);
 	wcnt = write(ocnt, text_content, cnt);
