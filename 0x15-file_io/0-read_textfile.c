7394f19ae2bd0a9c99c9617bf5bd900ae544180c
chukssomzzy <chukssomzzy19@gmail.com>
2022-11-01 10:53:47 +0100

    omoh

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,14 +14,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, cnt, wrcnt;
 	char *buf = malloc(sizeof(char) * letters);
 
+	if (!buf)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1 || !filename)
+	{
+		free(buf);
 		return (0);
+	}
 	cnt = read(fd, buf, letters);
 	if (cnt == -1)
+	{
+		close(fd);
+		free(buf);
 		return (0);
+	}
 	wrcnt = write(STDIN_FILENO, buf, cnt);
 	if (wrcnt == -1 || wrcnt != cnt)
+	{
+		close(fd);
+		free(buf);
 		return (0);
+	}
+	close(fd);
+	free(buf);
 	return (wrcnt);
 }
