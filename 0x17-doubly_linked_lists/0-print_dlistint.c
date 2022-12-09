9576a62a03dc5c850a19a6e35295ddbeabfcde00
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-09 17:52:57 +0100

    recursive

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+# include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - print all the elements
+ * @h: poinyer to double pointer
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	if (!h->next)
+	{
+		printf("%i\n", h->n);
+		return (1);
+	}
+	printf("%i\n", h->n);
+	return (1 + print_dlistint(h->next));
+}
