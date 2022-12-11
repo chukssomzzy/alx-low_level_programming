96047d58c154df26f8d6461145e37e08ed646774
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-11 18:26:20 +0100

    made head-> = new

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,12 +15,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
-	new->n = n;
+	if (!n)
+		new->n = 0;
+	else
+		new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
 	if (head && !(*head))
 		return (*head = new);
 	new->next = *head;
+	(*head)->prev = new;
 	*head = new;
 	return (new);
 }
