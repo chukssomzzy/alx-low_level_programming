caed1dd0002268816fbdfeb70b843579e6346fa8
chukssomzzy <chukssomzzy@gmail.com>
2022-12-12 23:50:55 +0100

    dynamic library is fun

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,65 @@
+# include "operations.h"
+
+/**
+ * add - add two numbers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substract a from b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first numbers
+ * @b: second numbers
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div a by b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - a mod b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
