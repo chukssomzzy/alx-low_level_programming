ba33d7d4ba3d33e4f8ce53cb79f65cbfab42b3d2
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-12 06:33:12 +0100

    sum = 1534

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,29 +1,15 @@
-#include "lists.h"
+# include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n)
- * of a doubly linked list
+ * sum_dlistint - sum up n component of linked lists
+ * @head: pointer to double linked lists
  *
- * @head: head of the list
- * Return: sum of the data
+ * Return: sum_dlistint
  */
+
 int sum_dlistint(dlistint_t *head)
 {
-	int sum;
-
-	sum = 0;
-
-	if (head != NULL)
-	{
-		while (head->prev != NULL)
-			head = head->prev;
-
-		while (head != NULL)
-		{
-			sum += head->n;
-			head = head->next;
-		}
-	}
-
-	return (sum);
+	if (!head)
+		return (0);
+	return (head->n + sum_dlistint(head->next));
 }
