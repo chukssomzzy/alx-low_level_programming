1d9cb643ffb3d122a27ff9685a77a4db75345828
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-13 20:19:42 +0100

    after much work

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+# include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at index
+ * @h: pointer to head
+ * @idx: index to insert node
+ * @n: node to insert
+ * Return: pointer to new node or NULL
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *tmp;
+	size_t i = 0;
+
+	if (idx && h && !(*h))
+		return (NULL);
+	tmp = *h;
+	while ((i != idx) && (tmp->next))
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (i != idx)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (!idx && h && !(*h))
+		return (*h = new);
+	if (!idx)
+	{
+		new->next = tmp;
+		tmp->prev = new;
+		return (*h = new);
+	}
+	tmp->prev->next = new;
+	new->next = tmp;
+	new->prev = tmp->prev;
+	tmp->prev = new;
+	return (new);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,10 +13,11 @@
  * Description: doubly linked list node structure
  *
  */
-typedef struct dlistint_s {
-  int n;
-  struct dlistint_s *prev;
-  struct dlistint_s *next;
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t dlistint_len(const dlistint_t *h);
@@ -26,5 +27,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 # endif
