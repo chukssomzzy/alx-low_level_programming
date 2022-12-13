e3d9bb1a62f0174c461a31777840673502f96aaf
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-13 23:47:21 +0100

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if ((idx && h && !(*h)) || !h)
 		return (NULL);
 	tmp = *h;
+	while (!(tmp->prev))
+		tmp = tmp->prev;
 	while ((i != idx) && (tmp->next))
 	{
 		tmp = tmp->next;
