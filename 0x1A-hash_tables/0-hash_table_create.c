325cffede4572ca6ce9332487bc410f1131886d5
chukssomzzy <chukssomzzy19@gmail.com>
2023-01-06 23:55:10 +0100

    hash table create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+# include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - create a hash table
+ * @size: size of hash table
+ * Return: NULL or hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(size);
+	if (!table)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	while (i < size)
+		*(table->array + i++) = NULL;
+	return (table);
+}
