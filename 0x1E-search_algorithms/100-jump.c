06bd210bb1146053adf0ad34754db78d75fea9ea
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-10 17:46:53 +0100

    work in progress

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,49 @@
+# include "search_algos.h"
+# include <math.h>
+#include <stdio.h>
+
+/**
+ * jump_search - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array to search
+ * @size: the number of elements in array
+ * @value: value to searcg
+ * Return: first index where value is located
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t par_lft = 0;
+	size_t jmp_sz = sqrt(size);
+	size_t par_rht = jmp_sz;
+
+	while (par_rht < size)
+		if (array[par_rht] < value)
+		{
+			printf("Value checked array[%lu] = [%i]\n", par_lft, array[par_lft]);
+			printf("Value checked array[%lu] = [%i]\n", par_rht, array[par_rht]);
+			par_lft = par_rht;
+			par_rht = par_lft + jmp_sz;
+		} else if (array[par_rht] == value)
+		{
+			printf("Value checked array[%lu] = [%i]\n", par_rht, array[par_rht]);
+			return (par_rht);
+		} else if (array[par_lft] == value)
+		{
+			printf("Value checked array[%lu] = [%i]\n", par_rht, array[par_rht]);
+			return (par_lft);
+		}
+	if (par_rht < size)
+		printf("Value found between indexes [%lu] and [%lu]\n", par_lft, par_rht);
+	else
+		printf("Value found between indexes [%lu] and [%lu]\n", par_lft, size);
+	while (par_lft < size)
+	{
+		if (array[par_lft] == value)
+		{
+			printf("Value checked array[%lu] = [%i]\n", par_rht, array[par_rht]);
+			return (par_lft);
+		}
+		par_lft++;
+	}
+	return (-1);
+}
